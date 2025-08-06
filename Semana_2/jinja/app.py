d8ffa57321ad090f1e749a928895e0ec2b1e04ad
Isaias Swidzinski Junior <swidzinski.isaias.junior@gmail.com>
2025-08-06 23:05:57 +0000

    Yessss

--- Semana_2/jinja/app.py
+++ Semana_2/jinja/app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     usuario = "João Cana Brava"
+    return render_template('index.html', nome_usuario = usuario)
+
+@app.route('/perfil/<nome>')
+def perfil(nome=None):
+    logado = True
+    usuario = nome
+    return render_template('perfil.html', logado = logado, nome_usuario = usuario)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0" port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
